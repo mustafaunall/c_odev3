64c19f582502b4f724919bcc4e3da6d545d9cf1a
RevengeFire <39712938+RevengeFire@users.noreply.github.com>
2018-11-01 17:41:57 +0300

    Create 1.soru(palindrome).c

---
+++ 1.soru(palindrome).c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int sayi, gecici, sonuc = 0, kalan;
+	printf("Sayi Giriniz: ");
+	scanf("%d", &sayi);
+	gecici = sayi;
+	while (gecici > 0) {
+			kalan = gecici % 10;
+			sonuc = sonuc * 10 + kalan;
+			gecici /= 10;
+		}
+	if (sayi == sonuc) printf("Sayı Palindrom'dur.");
+	else printf("Sayı Palindrom değildir!");
+}
