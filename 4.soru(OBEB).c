69a7ba7013c781f54a3b1e5a7ef4b0d2be21a183
RevengeFire <39712938+RevengeFire@users.noreply.github.com>
2018-11-01 14:59:19 +0300

    Update 4.soru(OBEB).c

--- 4.soru(OBEB).c
+++ 4.soru(OBEB).c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 void main() {
-	int a, b, x, obeb;
-	printf("obeb icin ilk sayi gir : "); scanf("%d", &a); // ilk sayıyı a'ya atadım
-	printf("obeb icin ikinci sayi gir : "); scanf("%d", &b); // ikinci sayıyı b'ye atadım
-	if (a > b) x = a; else x = b; // büyük olanı x'e atadım
+	int ilksayi, ikincisayi, x, obeb;
+	printf("obeb icin ilk sayi gir : "); scanf("%d", &ilksayi); // ilk sayıyı a'ya atadım
+	printf("obeb icin ikinci sayi gir : "); scanf("%d", &ikincisayi); // ikinci sayıyı b'ye atadım
+	if (ilksayi > ikincisayi) x = ilksayi; else x = ikincisayi; // büyük olanı x'e atadım
 	for (int i = x; i >= 1; i--) // büyük sayıdan 1 e kadar kalansız bölünme test ettim
 	{
-		if ((a%i == 0) && (b%i == 0)) { obeb = i; break; } else continue;
+		if ((ilksayi%i == 0) && (ikincisayi%i == 0)) { obeb = i; break; } else continue;
 	}
 	printf("OBEB = %d", obeb); // OBEB'i yazdırdım
 }
