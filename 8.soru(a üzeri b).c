f7a1c997597fa6c947bdca5813a44af2dc2bccd9
RevengeFire <39712938+RevengeFire@users.noreply.github.com>
2018-11-01 16:39:34 +0300

    Update 8.soru(a üzeri b).c

--- 8.soru(a üzeri b).c
+++ 8.soru(a üzeri b).c
@@ -6,12 +6,12 @@
 int main() {
 	setlocale(LC_ALL, "Turkish");
 	int a, b, a_ilk;
-	printf("a uzeri b icin a girin : "); scanf("%d", &a);
-	printf("a uzeri b icin b girin : "); scanf("%d", &b);
+	printf("A Üzeri B için A Girin : "); scanf("%d", &a);
+	printf("A Üzeri B için B Girin : "); scanf("%d", &b);
 	a_ilk = a;
 	for (int i = 1; i < b; i++)
 	{
 		a = a * a_ilk;
 	}
-	printf("%d uzeri %d = %d", a_ilk, b, a);
+	printf("%d Üzeri %d = %d", a_ilk, b, a);
 }
